f0319378eb71dc5f1e17ec09e10b8aef9f73c6a3
Matthew Butt <matthew@matthewbutt.com>
2012-02-29 23:54:30 +0000

    Pointed postURL to correct port

--- content/home.js
+++ content/home.js
@@ -1,6 +1,6 @@
 Picup.convertFileInput(document.getElementById('image'), {
 	referrername: escape('Picup plus node.js'),
 	purpose: escape('Testing'),
-	postURL: escape('http://192.168.1.71/images/'),
+	postURL: escape('http://192.168.1.71:1337/images/'),
 	returnServerResponse: true
 })

cbf1ef78c0808aedf91247cfd04858ee3a62e18f
Matthew Butt <matthew@matthewbutt.com>
2012-02-29 23:29:01 +0000

    Added files for picup test

---
+++ content/home.js
@@ -0,0 +1,6 @@
+Picup.convertFileInput(document.getElementById('image'), {
+	referrername: escape('Picup plus node.js'),
+	purpose: escape('Testing'),
+	postURL: escape('http://192.168.1.71/images/'),
+	returnServerResponse: true
+})

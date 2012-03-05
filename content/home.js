c0e7f4e75af14cd8786093860c920132f466f020
Matthew Butt <matthew@matthewbutt.com>
2012-03-05 23:44:48 +0000

    Run JS on load (so picup has loaded); sniff for iPhone/iPad

--- content/home.js
+++ content/home.js
@@ -1,6 +1,10 @@
-Picup.convertFileInput(document.getElementById('image'), {
-	referrername: escape('Picup plus node.js'),
-	purpose: escape('Testing'),
-	postURL: escape('http://192.168.1.71:1337/images/'),
-	returnServerResponse: true
-})
+window.onload = function(){
+	if (navigator.userAgent && (~navigator.userAgent.indexOf('iPhone') || ~navigator.userAgent.indexOf('iPad'))) {
+		Picup.convertFileInput(document.getElementById('image'), {
+			referrername: escape('Picup plus node.js'),
+			purpose: escape('Testing'),
+			postURL: escape('http://192.168.1.71:1337/images/'),
+			returnServerResponse: true
+		})
+	}
+}

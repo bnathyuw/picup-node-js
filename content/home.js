86cc972f6dc4277318f07c4dfa9b6092dc8f7cf8
Matthew Butt <matthew@matthewbutt.com>
2012-03-06 00:06:04 +0000

    Started setting up callback

--- content/home.js
+++ content/home.js
@@ -1,9 +1,16 @@
 window.onload = function(){
 	if (navigator.userAgent && (~navigator.userAgent.indexOf('iPhone') || ~navigator.userAgent.indexOf('iPad'))) {
+		window.name = "picup_test"
+		
+		Picup.callbackHandler = function(params) {
+			console.log(params)
+		}
+		
 		Picup.convertFileInput(document.getElementById('image'), {
 			referrername: escape('Picup plus node.js'),
 			purpose: escape('Testing'),
 			postURL: escape('http://192.168.1.71:1337/images/'),
+			callbackURL: escape('http://192.168.1.71:1337/callback.html'),
 			returnServerResponse: true
 		})
 	}
